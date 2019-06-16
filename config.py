62223aec068215a0f4b0f99006a2627f77005949
Andrii Doroshenko (Xrayez) <xrayez@gmail.com>
2019-06-16 20:13:28 +0300

    Import platform-specific defines from config

--- config.py
+++ config.py
@@ -6,3 +6,13 @@ def can_build(env, platform):
 
 def configure(env):
     pass
+
+def get_defines(env):
+    if env["platform"] == "android":
+        return {
+            'PJ_ANDROID' : 1,
+            'PJ_M_NAME' : '\\\"' + env['android_arch'] + '\\\"',
+            'PJ_IS_LITTLE_ENDIAN' : 1,
+            'PJ_IS_BIG_ENDIAN' : 0,
+        }
+    
